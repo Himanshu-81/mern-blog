ce4b1bb77f9be7cfe844b034ef4ea225e036d972
Himanshu <hc200108@gmail.com>
2024-03-20 15:26:08 +0530

    changes in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "https://mern-blog-frontend-rho-sooty.vercel.app/",
+    origin: "http://localhost:5173",
+    methods: "GET,PUT,POST,DELETE, PATCH",
   })
 );
 
