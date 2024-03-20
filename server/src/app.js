d02424c081cb12880cc139b1f6791b657e371935
Himanshu <hc200108@gmail.com>
2024-03-20 14:44:53 +0530

    changes the cors url

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://https://mern-blog-frontend-rho-sooty.vercel.app/",
+    origin: "https://mern-blog-frontend-rho-sooty.vercel.app/",
   })
 );
 
