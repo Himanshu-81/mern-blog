08a66ffc7dff5834c34ff51cbf92ebe59033669c
Himanshu <128717694+Himanshu-81@users.noreply.github.com>
2024-03-21 09:20:00 +0530

    Update Posts.jsx

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -16,7 +16,14 @@ const Posts = () => {
     const getPosts = async () => {
       try {
         setLoading(true);
-        const posts = await axios.get(`${import.meta.env.VITE_BASE_URL}/posts`);
+        const response = await axios.get(
+  `${import.meta.env.VITE_BASE_URL}/posts`,
+  {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+);
         setLoading(false);
         const postsData = posts.data.data.map((post) => ({
           ...post,
