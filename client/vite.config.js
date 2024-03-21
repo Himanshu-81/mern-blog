102b3834fd91e3c24c14f2a10f4760c76a83b5f3
Himanshu <128717694+Himanshu-81@users.noreply.github.com>
2024-03-21 08:43:11 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mern-blog-kappa-ten.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
