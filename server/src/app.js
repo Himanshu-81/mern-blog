25d8b79a264cfc163599ce64e653a905c2465d74
Himanshu <128717694+Himanshu-81@users.noreply.github.com>
2024-03-21 08:53:14 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "https://mern-blog-frontend-umber.vercel.app/api",
+    origin: "https://mern-blog-frontend-umber.vercel.app",
     methods: ["GET", "PUT", "POST", "DELETE", "PATCH"]
   })
 );
